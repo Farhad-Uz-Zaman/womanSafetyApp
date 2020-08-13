64dda1bb5276cb727a104c3781894e1630f84659
Chisty-nightmare <maruhasan97@gmail.com>
2020-08-13 23:40:01 +0600

    login-logout

--- app/src/main/java/com/example/womansafetyapp/loginActivity.java
+++ app/src/main/java/com/example/womansafetyapp/loginActivity.java
@@ -1,40 +1,100 @@
 package com.example.womansafetyapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class loginActivity extends AppCompatActivity {
+    private EditText email;
+    private EditText passWord;
+    private Button login;
+    private FirebaseAuth authenticate;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        email=(EditText)findViewById(R.id.email);
+        passWord=(EditText)findViewById(R.id.Password);
+        login = (Button) findViewById(R.id.login);
 
-        Button login = (Button) findViewById(R.id.login);
+        authenticate= FirebaseAuth.getInstance();
+
+
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent mainIntent = new Intent(loginActivity.this, MainActivity.class);
-                startActivity(mainIntent);
-                finish();
+
+                if(getData()){
+                    String logEmail=email.getText().toString();
+                    String logPass=passWord.getText().toString();
+                    userLogin(logEmail,logPass);
+               }
             }
         });
+
+
 
         Button signup = (Button) findViewById(R.id.signup);
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent mainIntent = new Intent(loginActivity.this, SignupActivity.class);
-                startActivity(mainIntent);
+                Intent intent= new Intent(loginActivity.this, SignupActivity.class);
+                startActivity(intent);
                 finish();
             }
         });
     }
+    private void userLogin(String email,String password)
+    {
+        authenticate.signInWithEmailAndPassword(email,password).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(loginActivity.this,"Login failed",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        authenticate.signInWithEmailAndPassword(email,password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+            @Override
+            public void onSuccess(AuthResult authResult) {
+                Toast.makeText(loginActivity.this,"Logged in",Toast.LENGTH_SHORT).show();
+                Intent intent= new Intent(loginActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+    }
+
+    private boolean getData()
+    {
+        String loginEmail= email.getText().toString();
+        String loginPassWord= passWord.getText().toString();
+
+        if(loginEmail.isEmpty()||loginPassWord.isEmpty())
+        {
+            Toast.makeText(loginActivity.this,"Please Complete the fields",Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
+    }
+
+
 
 
 }
--- app/src/main/java/com/example/womansafetyapp/settings.java
+++ app/src/main/java/com/example/womansafetyapp/settings.java
@@ -1,5 +1,6 @@
 package com.example.womansafetyapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,22 +8,46 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class settings extends Fragment {
+
+public class settings extends Fragment implements View.OnClickListener {
+    private Button signout;
+    View view;
 
     public settings() {
         // Required empty public constructor
     }
 
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+        view=inflater.inflate(R.layout.fragment_settings, container, false);
+        signout=(Button)view.findViewById(R.id.signout);
+        signout.setOnClickListener(this);
+        return view;
     }
+    @Override
+    public void onClick(View v) {
+        FirebaseAuth.getInstance().signOut();
+        Toast.makeText(getActivity(),"Signed Out",Toast.LENGTH_SHORT).show();
+        Intent intent= new Intent(getActivity(), loginActivity.class);
+        startActivity(intent);
+        //finish();
+
+
+    }
+
+
+
 }

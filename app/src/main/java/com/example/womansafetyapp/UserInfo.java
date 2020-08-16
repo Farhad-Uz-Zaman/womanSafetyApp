e11f00c57f3f24fcc2d7976be98227bb962ef970
Farhad <farhaduzzaman069@gmail.com>
2020-08-17 02:10:33 +0600

    Login User Data fetch done (Without showing in main activity, testactivity used )

---
+++ app/src/main/java/com/example/womansafetyapp/UserInfo.java
@@ -0,0 +1,22 @@
+package com.example.womansafetyapp;
+
+public class UserInfo {
+
+    public String Name,Username,Mail,Address,Phone;
+
+    public UserInfo(){
+
+    }
+
+    public UserInfo(String Name,String Username,String Mail,String Address,String Phone){
+        this.Name=Name;
+        this.Username=Username;
+        this.Mail=Mail;
+        this.Address=Address;
+        this.Phone=Phone;
+
+    }
+
+
+
+}
--- app/src/main/java/com/example/womansafetyapp/loginActivity.java
+++ app/src/main/java/com/example/womansafetyapp/loginActivity.java
@@ -10,16 +10,25 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class loginActivity extends AppCompatActivity {
     private EditText email;
     private EditText passWord;
     private Button login;
+    private Button signup;
     private FirebaseAuth authenticate;
+    private DatabaseReference ref;
 
 
 
@@ -29,11 +38,16 @@ public class loginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+
+
+
         email=(EditText)findViewById(R.id.email);
         passWord=(EditText)findViewById(R.id.Password);
         login = (Button) findViewById(R.id.login);
 
         authenticate= FirebaseAuth.getInstance();
+        ref= FirebaseDatabase.getInstance().getReference().child("Users");
+
 
 
         login.setOnClickListener(new View.OnClickListener() {
@@ -44,13 +58,13 @@ public class loginActivity extends AppCompatActivity {
                     String logEmail=email.getText().toString();
                     String logPass=passWord.getText().toString();
                     userLogin(logEmail,logPass);
-               }
+                }
             }
         });
 
 
 
-        Button signup = (Button) findViewById(R.id.signup);
+         signup = (Button) findViewById(R.id.signup);
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -60,24 +74,65 @@ public class loginActivity extends AppCompatActivity {
             }
         });
     }
-    private void userLogin(String email,String password)
+    private void userLogin( String email, String password)
     {
-        authenticate.signInWithEmailAndPassword(email,password).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                Toast.makeText(loginActivity.this,"Login failed",Toast.LENGTH_SHORT).show();
-            }
-        });
 
-        authenticate.signInWithEmailAndPassword(email,password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
-            @Override
-            public void onSuccess(AuthResult authResult) {
-                Toast.makeText(loginActivity.this,"Logged in",Toast.LENGTH_SHORT).show();
-                Intent intent= new Intent(loginActivity.this, MainActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        });
+        authenticate.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+
+                            Toast.makeText(loginActivity.this,"Logged In",Toast.LENGTH_SHORT).show();
+
+                            ref.addValueEventListener(new ValueEventListener() {
+
+
+                                @Override
+                                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                                    for(DataSnapshot dataSnapshot1 : dataSnapshot.getChildren()){
+
+                                        UserInfo  userinfo = dataSnapshot1.getValue(UserInfo.class);
+
+
+                                        Intent intent= new Intent(loginActivity.this, TestActivity.class);
+                                        intent.putExtra("NAME",userinfo.Name);
+                                        intent.putExtra("USERNAME",userinfo.Username);
+                                        intent.putExtra("EMAIL",userinfo.Mail);
+                                        intent.putExtra("PHONE",userinfo.Phone);
+                                        intent.putExtra("ADDRESS",userinfo.Address);
+                                        startActivity(intent);
+                                        finish();
+
+
+
+                                    }
+
+
+                                }
+
+                                @Override
+                                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                                }
+                            });
+
+
+
+
+                                } else {
+
+                            Toast.makeText(loginActivity.this,"Loggin Failed",Toast.LENGTH_SHORT).show();
+
+                        }
+
+
+                    }
+                });
+
+
+
 
     }
 
